b1707b6b69a6624e9e15590d45a102f3acaa5c21
Tommaso Celano <vecchiotom1@gmail.com>
2025-07-01 00:03:50 +0200

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const getDocuments = require('./app');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DOCUMENTS_FILE = path.join(__dirname, 'documents.json');
 
 // Middleware to parse JSON bodies
